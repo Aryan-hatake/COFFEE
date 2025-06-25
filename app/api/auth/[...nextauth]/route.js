ba267777fa2e94b2e5235a4987451298b908ac51
Aryan <aryangadhave202@gmail.com>
2025-06-25 21:48:31 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -26,8 +26,8 @@ export const providerss= NextAuth({
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
     // // Passwordless / email sign in
     // EmailProvider({
